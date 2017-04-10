99355be4c36918171f195151f79b3adb5513e46b
icevct <icevct@gmail.com>
2017-04-10 13:45:02 -0300

    falta terminar, so pra deixar ja aqui mesmo

---
+++ Grafo.py
@@ -0,0 +1,15 @@
+#TAD de grafos em python
+
+
+#classe que implementa as funcionalidades dos vertices individuais do grafo
+
+
+#classe do grafo geral em si, ou seja, a topologia
+class Grafo:
+	#construtor do grafo
+    def __init__:
+    	self.lista_vertices = {}
+    	self.num_vertices = 0
+
+    #metodo que vai adicionar um vertice no grafo
+    def adiciona_vertice(self, id_vertice):
