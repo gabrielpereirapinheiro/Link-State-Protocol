1a472710c474080a8b1437888e65cf3adfb17f0e
icevct <icevct@gmail.com>
2017-04-10 22:23:28 -0300

    TAD de grafos funfando em python

--- Grafo.py
+++ Grafo.py
@@ -29,39 +29,38 @@ class Vertice:
 #classe do grafo geral em si, ou seja, a topologia
 class Grafo:
 	#construtor do grafo
-    def __init__:
-    	self.lista_vertices = {}
-    	self.num_vertices = 0
+	def __init__(self):
+		self.lista_vertices = {}
+		self.num_vertices = 0
 
-    #metodo que vai adicionar um vertice no grafo
-    def adiciona_vertice(self, id_vertice):
-    	self.num_vertices = self.num_vertices + 1
-    	novo_vertice = Vertice(id_vertice)
-    	self.lista_vertices[id_vertice] = novo_vertice
-    	return novo_vertice
+	#metodo que vai adicionar um vertice no grafo
+	def adiciona_vertice(self, id_vertice):
+		self.num_vertices = self.num_vertices + 1
+		novo_vertice = Vertice(id_vertice)
+		self.lista_vertices[id_vertice] = novo_vertice
+		return novo_vertice
 
-    #metodo que retorna um vertice do grafo
-    def get_vertice(self, id_vertice):
-    	if id_vertice in self.lista_vertices:
-    		return self.lista_vertices[id_vertice]
-    	else:
-    		print "Nao existe o vertice no grafo!"
-    		return None
+	#metodo que retorna um vertice do grafo
+	def get_vertice(self, id_vertice):
+		if id_vertice in self.lista_vertices:
+			return self.lista_vertices[id_vertice]
+		else:
+			print "Nao existe o vertice no grafo!"
+			return None
 
-    #metodo que verifica se existe o vertice no grafo ou nao
-    #retorna True or False
-    def verifica_se_existe_no_grafo(self, 	id_vertice):
-    	return n in self.lista_vertices
+	#metodo que verifica se existe o vertice no grafo ou nao
+	#retorna True or False
+	def verifica_se_existe_no_grafo(self, id_vertice):
+		return id_vertice in self.lista_vertices
 
-    #metodo que adiciona uma aresta entre os vertices x e y no grafo
-    #um grafo nao dirigido
-	def adiciona_vertice(self, x, y, peso = 0):
+	#metodo que adiciona uma aresta entre os vertices x e y no grafo
+	#um grafo nao dirigido
+	def adiciona_aresta(self, x, y, peso = 0):
 		if x not in self.lista_vertices:
-		    novo_vert_x = self.adiciona_vertice(self, x)
+		    novo_vert = self.adiciona_vertice(self, x)
 		if y not in self.lista_vertices:
-		    novo_vert_y = self.adiciona_vertice(self, y)
-
-		#grafo nao dirigido, ou seja, vai adicionar os vizinhos nos dois
+			novo_vert = self.adiciona_vertice(self, y)
+		    #grafo nao dirigido, ou seja, vai adicionar os vizinhos nos dois
 		self.lista_vertices[x].adiciona_vizinhos(self.lista_vertices[y], peso)
 		self.lista_vertices[y].adiciona_vizinhos(self.lista_vertices[x], peso)
 
@@ -69,6 +68,26 @@ class Grafo:
 	def get_vertices(self):
 		return self.lista_vertices.keys()
 
-	
-	def __iter__(self):
-		return iter(self.lista_vertices.values())
+
+g = Grafo()
+
+g.adiciona_vertice('V0')
+g.adiciona_vertice('V1')
+g.adiciona_vertice('V2')
+g.adiciona_vertice('V3')
+g.adiciona_vertice('V4')
+g.adiciona_vertice('V5')
+
+g.adiciona_aresta('V0', 'V1', 5)
+g.adiciona_aresta('V0', 'V4', 1)
+g.adiciona_aresta('V0', 'V5', 2)
+g.adiciona_aresta('V1', 'V2', 4)
+g.adiciona_aresta('V2', 'V3', 9)
+g.adiciona_aresta('V2', 'V5', 1)
+g.adiciona_aresta('V3', 'V5', 3)
+g.adiciona_aresta('V3', 'V4', 7)
+g.adiciona_aresta('V4', 'V5', 8)
+
+for v in g.lista_vertices:
+	for w in g.get_vertice(v).get_conexoes():
+		print ("(%s, %s)" % (g.get_vertice(v).get_id(), w.get_id()))
